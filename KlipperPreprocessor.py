5bc6bfbb52abbc0330e79984b1506d8994778af2
Pedro Lamas <pedrolamas@gmail.com>
2023-07-06 09:18:21 +0100

    Refactor and adds some typings

--- KlipperPreprocessor.py
+++ KlipperPreprocessor.py
@@ -9,6 +9,7 @@ from ..Script import Script
 from UM.Logger import Logger
 from UM.Message import Message
 from tempfile import TemporaryDirectory
+from typing import List, Optional, Tuple
 import sys
 import subprocess
 import shutil
@@ -19,7 +20,7 @@ class KlipperPreprocessor(Script):
     Prepare resulting gcode for Klipper.
     """
 
-    def getSettingDataString(self):
+    def getSettingDataString(self) -> str:
         return """{
             "name": "Klipper Preprocessor v1.3.1",
             "key": "KlipperPreprocessor",
@@ -131,83 +132,101 @@ class KlipperPreprocessor(Script):
             }
         }"""
 
-    def execute(self, data):
-        add_set_print_stats_info = self.getSettingValueByKey("add_set_print_stats_info")
-        add_timelapse_take_frame = self.getSettingValueByKey("add_timelapse_take_frame")
-
+    def execute(self, data: List[str]) -> List[str]:
         with TemporaryDirectory() as work_dir:
-            Logger.log("d", "Initial run...")
-
-            total_layers = 0
-
-            filename = os.path.join(work_dir, "work.gcode")
-            with open(filename, 'w') as work_file:
-                first_layer_done = False
-                for layer in data:
-                    if first_layer_done:
-                        work_file.write(";CURA_DATA_SPLIT_HERE\n")
-                    else:
-                        first_layer_done = True
-                    lines = layer.split("\n")
-                    for line in lines:
-                        if (line.startswith(';LAYER:')):
-                            total_layers += 1
-                            if add_timelapse_take_frame:
-                                work_file.write("TIMELAPSE_TAKE_FRAME\n")
-                        work_file.write(line + "\n")
-
-            Logger.log("d", "Total layers found: %d", total_layers)
+            filename, total_layers = self.prepare_temp_file(data, work_dir)
 
             self.execute_preprocess_cancellation(filename)
 
             self.execute_klipper_estimator(filename, work_dir)
 
-            Logger.log("d", "Return output...")
+            return self.return_processed_data(filename, total_layers)
 
-            data = []
-            current_layer = 0
-            with open(filename) as work_file:
-                layer = []
-                for line in work_file:
-                    line = line.strip()
-                    if line == ";CURA_DATA_SPLIT_HERE":
-                        data.append("\n".join(layer))
-                        layer = []
-                    else:
-                        layer.append(line)
+    def prepare_temp_file(self, data: List[str], work_dir: str) -> Tuple[str, int]:
+        Logger.log("d", "Initial run...")
 
-                        if add_set_print_stats_info:
-                            if (line.startswith(';LAYER:')):
-                                current_layer += 1
-                                layer.append("SET_PRINT_STATS_INFO CURRENT_LAYER=%s" % (current_layer,))
-                            elif (line.startswith(';LAYER_COUNT:')):
-                                layer.append("SET_PRINT_STATS_INFO TOTAL_LAYER=%s" % (total_layers,))
-                data.append("\n".join(layer))
+        add_timelapse_take_frame: bool = self.getSettingValueByKey("add_timelapse_take_frame")
 
-            return data
+        filename = os.path.join(work_dir, "work.gcode")
+        total_layers = 0
 
-    def execute_preprocess_cancellation(self, filename):
-        if self.getSettingValueByKey("preprocess_cancellation_enabled"):
+        with open(filename, 'w') as work_file:
+            for index, layer in enumerate(data):
+                if index:
+                    work_file.write(";CURA_DATA_SPLIT_HERE\n")
+
+                lines = layer.split("\n")
+
+                for line in lines:
+                    if line.startswith(';LAYER:'):
+                        total_layers += 1
+                        if add_timelapse_take_frame:
+                            work_file.write("TIMELAPSE_TAKE_FRAME\n")
+                    work_file.write(line + "\n")
+
+        Logger.log("d", "Total layers found: %d", total_layers)
+
+        return filename, total_layers
+
+    def return_processed_data(self, filename: str, total_layers: int) -> List[str]:
+        Logger.log("d", "Return output...")
+
+        add_set_print_stats_info: bool = self.getSettingValueByKey("add_set_print_stats_info")
+
+        data: List[str] = []
+        current_layer = 0
+
+        with open(filename) as work_file:
+            layer: List[str] = []
+
+            for line in work_file:
+                line = line.strip()
+
+                if line == ";CURA_DATA_SPLIT_HERE":
+                    data.append("\n".join(layer))
+                    layer = []
+                else:
+                    layer.append(line)
+
+                    if add_set_print_stats_info:
+                        if line.startswith(';LAYER:'):
+                            current_layer += 1
+                            layer.append("SET_PRINT_STATS_INFO CURRENT_LAYER=%s" % (current_layer,))
+                        elif line.startswith(';LAYER_COUNT:'):
+                            layer.append("SET_PRINT_STATS_INFO TOTAL_LAYER=%s" % (total_layers,))
+            data.append("\n".join(layer))
+
+        return data
+
+    def execute_preprocess_cancellation(self, filename: str) -> None:
+        preprocess_cancellation_enabled: bool = self.getSettingValueByKey("preprocess_cancellation_enabled")
+
+        if preprocess_cancellation_enabled:
             Logger.log("d", "Running preprocess_cancellation...")
 
+            preprocess_cancellation_path: str = self.getSettingValueByKey("preprocess_cancellation_path")
+
             args = [
-                self.getSettingValueByKey("preprocess_cancellation_path"),
+                preprocess_cancellation_path,
                 filename,
             ]
-            timeout = self.getSettingValueByKey("preprocess_cancellation_timeout")
+            timeout: int = self.getSettingValueByKey("preprocess_cancellation_timeout")
 
             try:
                 ret = subprocess.run(args, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, startupinfo = self.getSubprocessStartupinfo(), timeout = timeout)
                 if ret.returncode != 0:
-                    self.showWarningMessage("Failed to run preprocess_cancellation\n%s" % (ret.stdout,))
+                    self.showWarningMessage("Failed to run preprocess_cancellation\n%s" % (ret.stdout.decode().strip(),))
             except subprocess.TimeoutExpired:
                 self.showWarningMessage("Timeout while running preprocess_cancellation")
 
-    def execute_klipper_estimator(self, filename, work_dir):
-        if self.getSettingValueByKey("klipper_estimator_enabled"):
-            klipper_estimator_config_type = self.getSettingValueByKey("klipper_estimator_config_type")
-            klipper_estimator_moonraker_url = self.getSettingValueByKey("klipper_estimator_moonraker_url")
-            klipper_estimator_config_file_path = self.getSettingValueByKey("klipper_estimator_config_file_path")
+    def execute_klipper_estimator(self, filename: str, work_dir: str) -> None:
+        klipper_estimator_enabled: bool = self.getSettingValueByKey("klipper_estimator_enabled")
+
+        if klipper_estimator_enabled:
+            klipper_estimator_config_type: str = self.getSettingValueByKey("klipper_estimator_config_type")
+            klipper_estimator_moonraker_url: str = self.getSettingValueByKey("klipper_estimator_moonraker_url")
+            klipper_estimator_config_file_path: str = self.getSettingValueByKey("klipper_estimator_config_file_path")
+            klipper_estimator_path: str = self.getSettingValueByKey("klipper_estimator_path")
 
             if klipper_estimator_config_type == 'moonraker_url' and self.getSettingValueByKey("klipper_estimator_config_cache"):
                 Logger.log("d", "Running klipper_estimator to get config from Moonraker...")
@@ -215,13 +234,14 @@ class KlipperPreprocessor(Script):
                 klipper_estimator_config_type = 'file'
 
                 args = [
-                    self.getSettingValueByKey("klipper_estimator_path"),
+                    klipper_estimator_path,
                     "--config_moonraker_url",
                     klipper_estimator_moonraker_url,
                     "dump-config"
                 ]
 
                 config_filename = os.path.join(work_dir, "config.json")
+
                 with open(config_filename, 'w') as config_file:
                     try:
                         ret = subprocess.run(args, stdout = config_file, startupinfo = self.getSubprocessStartupinfo(), timeout = 5)
@@ -232,25 +252,26 @@ class KlipperPreprocessor(Script):
 
             Logger.log("d", "Running klipper_estimator...")
 
-            klipper_estimator_config_arg = klipper_estimator_moonraker_url if klipper_estimator_config_type == 'moonraker_url' else klipper_estimator_config_file_path
+            klipper_estimator_config_arg: str = klipper_estimator_moonraker_url if klipper_estimator_config_type == 'moonraker_url' else klipper_estimator_config_file_path
 
             args = [
-                self.getSettingValueByKey("klipper_estimator_path"),
+                klipper_estimator_path,
                 "--config_" + klipper_estimator_config_type,
                 klipper_estimator_config_arg,
                 "post-process",
                 filename,
             ]
-            timeout = self.getSettingValueByKey("klipper_estimator_timeout")
+
+            timeout: int = self.getSettingValueByKey("klipper_estimator_timeout")
 
             try:
                 ret = subprocess.run(args, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, startupinfo = self.getSubprocessStartupinfo(), timeout = timeout)
                 if ret.returncode != 0:
-                    self.showWarningMessage("Failed to run klipper_estimator\n%s" % (ret.stdout,))
+                    self.showWarningMessage("Failed to run klipper_estimator\n%s" % (ret.stdout.decode().strip(),))
             except subprocess.TimeoutExpired:
                 self.showWarningMessage("Timeout while running klipper_estimator")
 
-    def getSubprocessStartupinfo(self):
+    def getSubprocessStartupinfo(self) -> Optional[subprocess.STARTUPINFO]:
         if sys.platform == "win32":
             startupinfo = subprocess.STARTUPINFO()
             startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
@@ -258,7 +279,7 @@ class KlipperPreprocessor(Script):
             return startupinfo
         return None
 
-    def showWarningMessage(self, text):
+    def showWarningMessage(self, text) -> None:
         Logger.logException("w", text)
 
         message = Message(text, title = "Klipper Preprocessor", message_type = Message.MessageType.WARNING)
