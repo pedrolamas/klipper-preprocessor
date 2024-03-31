3a6bdfde4bea748679efe411da043b820f026709
Pedro Lamas <pedrolamas@gmail.com>
2024-03-31 17:37:57 +0100

    Updates version to 1.3.3

--- KlipperPreprocessor.py
+++ KlipperPreprocessor.py
@@ -1,5 +1,5 @@
 # Klipper Preprocessor script for Cura
-# Version: 1.3.2
+# Version: 1.3.3
 #
 # Copyright (c) 2022 Lasse Dalegaard
 # Copyright (c) 2023 Pedro Lamas
@@ -22,7 +22,7 @@ class KlipperPreprocessor(Script):
 
     def getSettingDataString(self) -> str:
         return """{
-            "name": "Klipper Preprocessor v1.3.2",
+            "name": "Klipper Preprocessor v1.3.3",
             "key": "KlipperPreprocessor",
             "metadata": {},
             "version": 2,
